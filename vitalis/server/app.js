a3bdaefa93c22a110c9686cfcb32e1bf266b53d9
RiqueAlvess <henrique.alves.siqueira@hotmail.com>
2025-03-17 01:05:09 -0600

    Update app.js

--- vitalis/server/app.js
+++ vitalis/server/app.js
@@ -53,10 +53,12 @@ app.use('/api/planos', planosRoutes);
 
 // Servir arquivos estáticos do React em produção
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  const clientBuildPath = path.join(__dirname, '../client/build');
+  
+  app.use(express.static(clientBuildPath));
   
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    res.sendFile(path.join(clientBuildPath, 'index.html'));
   });
 }
 
